a3465639232d8b75111bb9050dced4827c4151da
Steve <steven.magelowitz@gmail.com>
2018-02-21 15:55:19 +0800

    use assigns (#1)

--- lib/gallows_web/controllers/page_controller.ex
+++ lib/gallows_web/controllers/page_controller.ex
@@ -2,6 +2,7 @@ defmodule GallowsWeb.PageController do
   use GallowsWeb, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    my_assigns = %{produce: "pear", count: 3}
+    render(conn, "index.html", my_assigns)
   end
 end
