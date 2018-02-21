47efd59b7017bd6169e09e9764717adbc0eff91d
Steven Magelowitz <steven.magelowitz@gmail.com>
2018-02-21 02:18:04 -0800

    add hangman graphic

--- lib/gallows_web/views/hangman_view.ex
+++ lib/gallows_web/views/hangman_view.ex
@@ -7,6 +7,14 @@ defmodule GallowsWeb.HangmanView do
     button("New Game", to: hangman_path(conn, :create_game))
   end
 
+  def turn(left, target) when target >= left do
+    "opacity: 1"
+  end
+
+  def turn(_left, _target) do
+    "opacity: 0.1"
+  end
+
   def game_over?(:lost), do: true
   def game_over?(:won), do: true
   def game_over?(_), do: false
